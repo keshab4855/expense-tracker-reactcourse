e0f351b40a6e1f9c42d76cc3bf1d43fbec731f51
keshab4855 <keshabroka123@gmail.com>
2022-06-13 18:07:15 +1000

    building components and implementing props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import "./components/ExpenseItem";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car insurance", amount: 294.98, date: new Date(2021, 2, 28) },
+    { title: "House maintanence", amount: 506.22, date: new Date(2018, 5, 5) },
+    { title: "Education", amount: 300.52, date: new Date(2020, 1, 22) },
+  ];
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
